b71dc9a036ae029e30dfbd4feaa9cedc085e6a19
Camille Junique <k23057058@kcl.ac.uk>
2024-03-26 20:31:41 +0000

    graph work

--- GraphPanel.java
+++ GraphPanel.java
@@ -1,10 +1,10 @@
 import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import java.net.URL;
 import javafx.scene.layout.Pane;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
@@ -12,25 +12,39 @@ import javafx.scene.Node;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ChoiceBox;
 import javafx.collections.FXCollections;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import java.util.ArrayList;
 
-public class GraphPanel extends Application {
+public class GraphPanel extends Application  {
     
     @FXML private ChoiceBox<String> graphChoice;
+    @FXML private BarChart<Integer, Integer>  barChart;
     @FXML private Button leftButton;
     @FXML private Button rightButton;
     private ObservableList<String> orderedBy;
     private String order;
+    
+    ArrayList<CovidData> data = new ArrayList<>();
+    DataManipulator dm = new DataManipulator();
     
     @FXML 
     public void initialize() {
         orderedBy = FXCollections.observableArrayList("Date", "Google Mobility Data", "New COVID cases", "Total COVID Cases", "New COVID Deaths");
         graphChoice.setItems(orderedBy);
         graphChoice.setValue("Date");
+        
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("Trends");
+        series1.getData().add(new XYChart.Data(dm.getTotalDeaths(), 2022));
+        series1.getData().add(new XYChart.Data(dm.getTotalDeaths(), 2022));
+        series1.getData().add(new XYChart.Data(dm.getTotalDeaths(), 2022));
+        
     }
     
     @FXML
     public void start(Stage stage) throws Exception{
-        URL url = getClass().getResource("graphPanel.fxml"); 
+        URL url = getClass().getResource("graph.fxml"); 
         Pane root = FXMLLoader.load(url); 
         Scene scene = new Scene(root); 
         stage.setTitle("Graph"); 
