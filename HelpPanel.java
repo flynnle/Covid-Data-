dbc147c2656c5766623d7204f461a0bfbcdb23b2
shankhi <shankhi.sinha@kcl.ac.uk>
2024-03-28 14:29:22 +0000

    help panel links

--- HelpPanel.java
+++ HelpPanel.java
@@ -1,3 +1,15 @@
+import java.net.URI;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import java.net.URL;
+import javafx.event.ActionEvent;
+import java.net.URISyntaxException;
+import java.io.IOException;
 
 /**
  * Write a description of class HelpPanel here.
@@ -5,19 +17,9 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class HelpPanel
+public class HelpPanel extends Application
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class HelpPanel
-     */
-    public HelpPanel()
-    {
-        // initialise instance variables
-        x = 0;
-    }
+    @FXML private Hyperlink nhslink;
 
     /**
      * An example of a method - replace this comment with your own
@@ -25,9 +27,18 @@ public class HelpPanel
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
+    public void start(Stage stage) throws Exception
     {
-        // put your code here
-        return x + y;
+        URL url = getClass().getResource("helpPanel.fxml"); 
+        Pane root = FXMLLoader.load(url); 
+        Scene scene = new Scene(root); 
+        stage.setTitle("Information"); 
+        stage.setScene(scene); 
+        stage.show();  
+    }
+    
+    public void openNHS(ActionEvent event) throws URISyntaxException, IOException{
+        URI uri = new URI("https://www.nhs.uk/conditions/covid-19/covid-19-symptoms-and-what-to-do/");
+        java.awt.Desktop.getDesktop().browse(uri);
     }
 }
